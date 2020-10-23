801d2270e8655ec234bb82b28d4cdbaa148fe10b
Andrey Samoilenko <a.samoile@gmail.com>
2020-10-23 20:16:42 +0600

    installed gulp

---
+++ js_less1/script.js
@@ -0,0 +1,5 @@
+// let x = 5; alert( x++ );
+// alert(y);
+// alert( "1"[0] )
+let y = 2 && 1 && null && 0 && undefined;
+console.log(y);
--- optimized/js/common.js
+++ optimized/js/common.js
@@ -1,5 +1,12 @@
-$(function() {
 
-	// Custom JS
 
+
+
+
+
+$(function () {
+  $('.logo-litera').each(function () {
+      var ths = $(this);
+      ths.html(ths.html().replace('O','<span>O</span>'));
+});
 });
